1177c448328f1ad73e09ab37edfe99ae85dcae56
Danilovich4 <121514623+Danilovich4@users.noreply.github.com>
2024-12-09 21:12:53 +0100

    Nueva funcionalidad

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-# Esto es un comentario
 """
-Este es un comentario
-de varias líneas
+Declare al menos 4 variables: una de cada tipo (int, float, str, bool).
+Realice una operación matemática con los números.
+Concatenar y mostrar un mensaje usando las cadenas.
+Incluya un comentario que explique cada variable.
 """
 
-# Este es mi primer programa
-print("Hola, mundo")
+# Variables
+nombre = "Dani"
+edad = 30
+altura = 1.84
+es_casado = False
+#Nuevas variables
+apellido = "Lomo"
+peso = 80
+otro_numero = 2.34
+activo = True
 
-# Pedir el nombre al usuario
-nombre = input("¿Cuál es tu nombre? ")
-# Pedir la edad al usuario
-edad = int(input("Cual es tu edad?: "))
-#Doble del numero dado
-edad_doble = int(edad*2)
+# Operaciones
+edad_doble = edad * 2
+saludo = "Hola, " + nombre
+# Operaciones nuevas
+calculo = peso / otro_numero
+concatenar = "El apellido es: " + apellido
 
-# Mostrar un mensaje personalizado
-print("Hola, " + nombre + ", ¡bienvenido a Python!")
-#Mostrar la edad y su doble
-print("Tu edad es " + str(edad) + " y el doble de esta es " + str(edad_doble))
+# Mostrar información
+print(saludo)
+print("Tienes", edad, "años y el doble de tu edad es", edad_doble)
+print("Tu altura es", altura, "metros")
+print("¿Estás casado?", es_casado)
+print(concatenar)
