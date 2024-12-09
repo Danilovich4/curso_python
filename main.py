e5d4379c78064d8061d7636b4ebc876cf31f8158
Danilovich4 <121514623+Danilovich4@users.noreply.github.com>
2024-12-09 20:18:35 +0100

    Primera funcionalidad

---
+++ main.py
@@ -0,0 +1,20 @@
+# Esto es un comentario
+"""
+Este es un comentario
+de varias líneas
+"""
+
+# Este es mi primer programa
+print("Hola, mundo")
+
+# Pedir el nombre al usuario
+nombre = input("¿Cuál es tu nombre? ")
+# Pedir la edad al usuario
+edad = int(input("Cual es tu edad?"))
+#Doble del numero dado
+edad_doble = int(edad*2)
+
+# Mostrar un mensaje personalizado
+print("Hola, " + nombre + ", ¡bienvenido a Python!")
+#Mostrar la edad y su doble
+print("Tu edad es " + edad + " y el doble de esta es " + edad_doble)
