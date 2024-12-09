f7e44da7f9aa42e6cef99c11af2fff9aace00856
Danilovich4 <121514623+Danilovich4@users.noreply.github.com>
2024-12-09 20:22:58 +0100

    Correcion errores

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ print("Hola, mundo")
 # Pedir el nombre al usuario
 nombre = input("¿Cuál es tu nombre? ")
 # Pedir la edad al usuario
-edad = int(input("Cual es tu edad?"))
+edad = int(input("Cual es tu edad?: "))
 #Doble del numero dado
 edad_doble = int(edad*2)
 
 # Mostrar un mensaje personalizado
 print("Hola, " + nombre + ", ¡bienvenido a Python!")
 #Mostrar la edad y su doble
-print("Tu edad es " + edad + " y el doble de esta es " + edad_doble)
+print("Tu edad es " + str(edad) + " y el doble de esta es " + str(edad_doble))
